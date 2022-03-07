58e479929b9fc4671de591b840ec3bbab0fe257c
Stepan Skryganov <skryganov@pay2u.ru>
2022-03-07 22:34:53 +0300

    SingleLinkedList: Removed unnecessary files Added missing refactoring to main.cpp

--- ch3/single/main.cpp
+++ ch3/single/main.cpp
@@ -3,25 +3,24 @@
 #include <string>
 using namespace std;
 
+const uint8_t REMOVE_TEST_COUNT = 5;
+
 int main() {
     SingleLinkedList<string> *list = new SingleLinkedList<string>();
-    SingleLinkedItem<string> a = SingleLinkedItem<string>("end");
+    SingleLinkedItem<string> addingItem = SingleLinkedItem<string>("end");
     
-    list->addFront(a);
-    a = SingleLinkedItem<string>("front");
-    list->addFront(a);
-    a = SingleLinkedItem<string>("popfront");
-    list->addFront(a);
-    cout<<(*list);
-    list->removeFront();
-    cout<<(*list);
-    list->removeFront();
-    cout<<(*list);
-    list->removeFront();
-    cout<<(*list);
-    list->removeFront();
-    cout<<(*list);
-    list->removeFront();
-    cout<<(*list);
+    list->addFront(addingItem);
+    addingItem = SingleLinkedItem<string>("front");
+    list->addFront(addingItem);
+    addingItem = SingleLinkedItem<string>("popfront");
+    list->addFront(addingItem);
+
+    for (auto i = REMOVE_TEST_COUNT; i > 0; --i) {
+        cout << (*list) << endl;
+        list->removeFront();
+    }
+
+    delete list;
+
     return 0;
 }
