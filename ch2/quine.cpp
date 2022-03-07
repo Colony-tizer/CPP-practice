0e3d1bbcab3d197a877daeebe73d7a4ee10c4735
Stepan Skryganov <skryganov@pay2u.ru>
2022-03-07 19:44:11 +0300

    Chapter 2: removed redundant space char

--- ch2/quine.cpp
+++ ch2/quine.cpp
@@ -1,2 +1,2 @@
 #include <cstdio>
-char *a="#include<cstdio>%c char *a=%c%s%c;int main(){printf(a,10,34,a,34);}";int main() {printf(a,10,34,a,34);}
+char *a="#include<cstdio>%c char *a=%c%s%c;int main(){printf(a,10,34,a,34);}";int main(){printf(a,10,34,a,34);}
