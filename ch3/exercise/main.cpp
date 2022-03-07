e1ad7578ea7e544235568c244ed2aecfb926c907
Stepan Skryganov <skryganov@pay2u.ru>
2022-03-08 00:17:58 +0300

    Ch.3. Exercise: Formatted code

--- ch3/exercise/main.cpp
+++ ch3/exercise/main.cpp
@@ -1,61 +1,93 @@
 #include <iostream>
-#include <math.h>
+#include <vector>
 #include <random>
-typedef float** matrix;
-typedef float* column;
-typedef float row;
+#include<cstdlib>
+#include<ctime>
 
-void printMatrix(const matrix mat, const size_t n, const size_t m) {
-    for (int i = 0; i < n; ++i) {
-        std::cout<<"M["<<i<<"]: ";
-        for (int j = 0; j < m; ++j) {
-            if (j != 0) std::cout<<", ";
-            std::cout<<mat[i][j];
+typedef float** Matrix2d;
+typedef float* Column;
+typedef float Row;
+
+void printMatrix(const Matrix2d mat, const size_t matrixSize) {
+    for (int i = 0; i < matrixSize; ++i) {
+        std::cout << "M[" << i << "]: ";
+        for (int j = 0; j < matrixSize; ++j) {
+            if (j != 0) std::cout << ", ";
+            std::cout << mat[i][j];
         }
-        std::cout<<std::endl;
+        std::cout << std::endl;
     }
-    std::cout<<std::endl;
+    std::cout << std::endl;
 }
-void transposeMatrix(const matrix mat, const size_t n, const size_t m) {
-    const size_t lastN = n-1, lastM = m-1;
-    for (int i = 0; i < n; ++i) {
-        for (int j = i+1; j < m; ++j) {
-            if (i == j) continue;
-            mat[lastN-i][lastM-j] += mat[i][j];
-            mat[i][j] = mat[lastN-i][lastM-j] - mat[i][j];
-            mat[lastN-i][lastM-j] -= mat[i][j];
+
+void transposeMatrix(const Matrix2d mat, const size_t matrixSize) {
+    const size_t lastN = matrixSize-1;
+    for (int i = 0; i < matrixSize; ++i) {
+        for (int j = i + 1; j < matrixSize; ++j) {
+            if (i != j) 
+            {            
+                mat[lastN-i][lastN-j] += mat[i][j];
+                mat[i][j] = mat[lastN-i][lastN-j] - mat[i][j];
+                mat[lastN-i][lastN-j] -= mat[i][j];
+            }
         }
     }
 }
-template <class T>
-void removeRandom(T *arr, const size_t len) {
-    if (len > 1) {
-        int randInd = std::rand()/((RAND_MAX+1u)/(len-1)) ;
-        T* newArr = new T(len-1);
-        for (int i = 0, j = 0; i < len-1; ++i, ++j) {
-            newArr[i] = (i == randInd) ? arr[++j] : arr[i];
-        }
-        delete arr;
-        arr = newArr;
-        removeRandom(arr, len-1);
-    } else delete arr;
-    
+
+size_t generateRandomIdx(const size_t maxIdx) {
+    auto divisor = (RAND_MAX + 1u) / (std::max(1, (int)maxIdx));
+    size_t res = std::rand() / divisor;
+
+    return res;
 }
+
+void removeRandomItemsUntilArrayEmpty(float *arr, const size_t arrSize) {
+    std::vector<size_t> activeIndices;
+    for (size_t i = 0; i < arrSize; ++i)
+        activeIndices.push_back(i);
+
+    auto activeIndicesCount = activeIndices.size();
+    do {
+        auto randIdx = generateRandomIdx(activeIndicesCount);
+        auto idxToRemove = activeIndices[randIdx];
+        
+        arr[idxToRemove] = NAN;
+        activeIndices.erase(activeIndices.begin() + randIdx);
+
+        --activeIndicesCount;
+    } while (activeIndicesCount > 0);   
+}
+
 int main() {
-    const size_t n = 3, m = 3;
-    matrix a = new column[n];
-    for (int i = 0; i < n; ++i) {
-        a[i] = new row[m];
-        for (int j = 0; j < m; ++j)
-            a[i][j] = (i+1)*(j+1) + j;
+    const size_t MATRIX_SIZE = 3;
+    Matrix2d matrix = new Column[MATRIX_SIZE];
+
+    for (int i = 0; i < MATRIX_SIZE; ++i) {
+        std::cout << "ROW " << i << std::endl;
+        matrix[i] = new Row[MATRIX_SIZE];
+        for (int j = 0; j < MATRIX_SIZE; ++j)
+            matrix[i][j] = (i + 1) * (j + 1) + j;
     }
     
-    printMatrix(a, n, m);
+    printMatrix(matrix, MATRIX_SIZE);
 
-    transposeMatrix(a, n, m);
+    transposeMatrix(matrix, MATRIX_SIZE);
     
-    printMatrix(a, n, m);
+    printMatrix(matrix, MATRIX_SIZE);
 
-    removeRandom(a[0], m);
+    // ----------------------------------------------
+
+    std::srand(std::time(nullptr));
+    size_t randRow = std::rand() / ((RAND_MAX + 1u)/(MATRIX_SIZE));
+
+    removeRandomItemsUntilArrayEmpty(matrix[randRow], MATRIX_SIZE);
+
+    printMatrix(matrix, MATRIX_SIZE);
+    
+
+    for (int i = 0; i < MATRIX_SIZE; ++i) {
+        delete[] matrix[i];
+    }
+    delete[] matrix;
 }
 
